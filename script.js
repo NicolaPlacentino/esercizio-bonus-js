8c490c9e1e529d76cbdf69beab1524291ab6e173
NicolaPlacentino <nyko0921@gmail.com>
2022-12-11 16:51:28 +0100

    completed exercise

--- script.js
+++ script.js
@@ -25,17 +25,45 @@ const button = document.getElementById('generate-button')
 const reset = document.getElementById('reset-button')
 
 button.addEventListener('click', function(){
-    const numberQuantity = outputQuantity.value
-    const numberMin = outputMin.value
-    const numberMax = outputMax.value
-    console.log(numberQuantity, numberMin, numberMax)
-
-    if (numberQuantity === '' || numberMin === '' || numberMax === ''){
+    const numberQuantity = parseInt(outputQuantity.value.trim())
+    const min = parseInt(outputMin.value.trim())
+    const max = parseInt(outputMax.value.trim())
+    console.log(numberQuantity, min, max)
+    
+    if (numberQuantity === '' || min === '' || max === ''){
         alert('Inserisci tutti i valori')
-    } else if (numberMin > numberMax){
+    } else if (isNaN(numberQuantity, min, max)) {
+        alert(' Inserisci dei valori numerici')
+    } else if (min > max){
         alert('Il minimo non può essere più grande del massimo')
-
-    } else {
         
+    } else {
+
+        outputArea.innerHTML = ''
+
+        for (let i = 1; i <= numberQuantity; i++) {
+            
+            const random = Math.random();
+            const randomNumber = Math.floor(random * (max - min + 1) + min);
+            let bgColor 
+            
+            if(randomNumber % 2 === 0){
+                bgColor = 'bg-primary'
+            } else {
+                bgColor = 'bg-danger'
+            }
+
+            outputArea.innerHTML += `<div class="col px-5"><span class="${bgColor} rounded-2 p-3 d-block" id="printed-number">${randomNumber}</span></div>`
+            
+            const printedNumber = document.getElementById('printed-number')
+            
+        }
     }
 });
+
+reset.addEventListener('click', function(){
+    outputQuantity.value = ''
+    outputMin.value = ''
+    outputMax.value = ''
+    outputArea.innerHTML = ''
+})
