42f16071e8170c387c228c3c84e9e6021b11416d
NicolaPlacentino <nyko0921@gmail.com>
2022-12-11 12:09:03 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+/* Scrivi un applicazione che sia in grado di generare una serie di numeri randomici.
+L'utente, tramite 3 input, deve avere la possibilità di definire:
+- Quanti box generare
+- il numero minimo di generazione dei numeri random
+- il numero massimo di generazione dei numeri random
+Cliccando sul tasto "genera", vengono generati i box con i parametri specificati
+dall'utente e andremo a colorare di VERDE i numeri pari e di ROSSO i numeri dispari.
+
+BONUS: 
+Create un bottone reset che svuota i valori negli input e cancella i box generati.*/
+
+// 1. 
